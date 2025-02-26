3672bb1295ecdb33b21efedd96e8382b59070f69
mariko85 <m.chaxvashvili@gmail.com>
2025-02-26 22:09:29 +0400

    burger meniu

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+
+
+let navigation = document.getElementById('navigation')
 let burger = document.getElementById('burger')
-let navigation =document.getElementById('navigation')
+
 burger.addEventListener('click', function(){
 if (navigation.classList.contains('activeNavigation')){
+    //ნავიგაციას აქრობს
     navigation.classList.remove('activeNavigation')
-    burger.innerHTML='<i class="fa-solid fa-bars"></i> '
+    //ბურგერ მენიუდ გადააქცევს x
+    burger.innerHTML= '<i class="fa-solid fa-bars"></i> '
 }
+
 else {
+    //გამოიტანს ნავიგაციის მენიუს
     navigation.classList.add('activeNavigation')
-    burger.innerHTML ='<i class="fa-regular fa-circle-xmark"></i>'
+    //გამოაჩენს x
+    burger.innerHTML ='<i class="fas fa-times"></i>'
 }
 
 })
