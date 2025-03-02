2fc9f40b080b24d6b1931fa1b1ad330d6695d84e
mariko85 <m.chaxvashvili@gmail.com>
2025-03-02 16:58:17 +0400

    about page

--- script.js
+++ script.js
@@ -19,3 +19,76 @@ else {
 }
 
 })
+
+
+let currentPage =1
+let totalPages
+
+function getUsers(page ){
+    fetch('https://reqres.in/api/users?page=' +page, {
+        method:'GET'
+    })
+
+    .then (function(response){
+        if(response.status !== 200){
+            throw response.status
+        }
+        return response.json()
+
+    })
+
+    .then (function(responseData){
+        let fragment =document.createDocumentFragment()
+        totalPages=responseData.total_pages
+         
+        responseData.data.forEach(function(item){
+            let li= document.createElement('li')
+            li.textContent=item.first_name + " " + item.last_name ;
+            let image =document.createElement('img')
+            image.src=item.avatar
+            fragment.appendChild(li)
+            fragment.appendChild(image)
+
+        })
+
+       document.getElementById('ul_list').innerHTML= " "
+       document.getElementById('ul_list').appendChild(fragment)
+
+    })
+
+  . catch (function(){
+    let conteiner= document.getElementById('conteiner')
+        let p = document.createElement('p')
+        p.textContent='error'
+        conteiner.appendChild(p)
+
+
+  }) 
+}
+
+
+
+
+  document.getElementById('loadprev').addEventListener('click',function(){
+   if(currentPage == 1){
+      return 
+    }
+    currentPage -=1
+    getUsers (currentPage)
+
+
+  })
+
+  
+  document.getElementById('loadnext').addEventListener('click',function(){
+   if  ( currentPage == totalPages){
+      return 
+    }
+    currentPage +=1
+    getUsers (currentPage)
+
+
+  })
+
+  getUsers(currentPage)
+  
